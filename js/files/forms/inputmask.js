847e9299778bed8316eb08cbc3de5f85d690c601
MikhailTravin <78166836+MikhailTravin@users.noreply.github.com>
2025-07-10 23:33:59 +0300

    Delete js directory

--- js/files/forms/inputmask.js
+++
@@ -1,7 +0,0 @@
-// Подключение модуля
-import "inputmask/dist/inputmask.min.js";
-
-const telephone = document.querySelectorAll('.telephone');
-if (telephone) {
-	Inputmask({ "mask": "+7 (999) - 999 - 99 - 99" }).mask(telephone);
-}
